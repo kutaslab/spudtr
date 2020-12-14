721e95f6d9b1dcc9c2be4d7477e7d3afaddb7842
docs_release_bot <docs_release_bot@the.cloud.org>
2020-12-14 18:53:12 +0000

    deploy type main python 3.8

---
+++ tests/test_epf.py
@@ -0,0 +1,490 @@
+import numpy as np
+import pandas as pd
+
+# local HDF5 files to be deprecated in v0.0.11 with _hdf_read_epochs
+from spudtr import DATA_DIR  # , P3_F, P5_F, WR_F
+
+# Zenodo archive feather files used starting with v0.0.9
+from spudtr import get_demo_df, WR_100_FEATHER, P5_1500_FEATHER
+from spudtr import epf
+import spudtr.fake_epochs_data as fake_data
+from spudtr.epf import EPOCH_ID, TIME
+
+import pytest
+
+# ------------------------------------------------------------
+# test epochs Zenodo https://doi.org/10.5281/zenodo.3968485
+# ------------------------------------------------------------
+WR_DF_COLS = [
+    "epoch_id",
+    "time_ms",
+    "sub_id",
+    "eeg_artifact",
+    "dblock_path",
+    "log_evcodes",
+    "log_ccodes",
+    "dblock_srate",
+    "ccode",
+    "instrument",
+    "repetition",
+    "pres_position",
+    "word_lag",
+    "congruity",
+    "event_code",
+    "condition_id",
+    "item_id",
+    "category",
+    "probe",
+    "lle",
+    "lhz",
+    "MiPf",
+    "LLPf",
+    "RLPf",
+    "LMPf",
+    "RMPf",
+    "LDFr",
+    "RDFr",
+    "LLFr",
+    "RLFr",
+    "LMFr",
+    "RMFr",
+    "LMCe",
+    "RMCe",
+    "MiCe",
+    "MiPa",
+    "LDCe",
+    "RDCe",
+    "LDPa",
+    "RDPa",
+    "LMOc",
+    "RMOc",
+    "LLTe",
+    "RLTe",
+    "LLOc",
+    "RLOc",
+    "MiOc",
+    "A2",
+    "HEOG",
+    "rle",
+    "rhz",
+]
+
+P5_DF_COLS = [
+    "epoch_id",
+    "time_ms",
+    "sub_id",
+    "eeg_artifact",
+    "dblock_path",
+    "log_evcodes",
+    "log_ccodes",
+    "dblock_srate",
+    "ccode",
+    "instrument",
+    "bin",
+    "click",
+    "type",
+    "lle",
+    "lhz",
+    "MiPf",
+    "LLPf",
+    "RLPf",
+    "LMPf",
+    "RMPf",
+    "LDFr",
+    "RDFr",
+    "LLFr",
+    "RLFr",
+    "LMFr",
+    "RMFr",
+    "LMCe",
+    "RMCe",
+    "MiCe",
+    "MiPa",
+    "LDCe",
+    "RDCe",
+    "LDPa",
+    "RDPa",
+    "LMOc",
+    "RMOc",
+    "LLTe",
+    "RLTe",
+    "LLOc",
+    "RLOc",
+    "MiOc",
+    "A2",
+    "HEOG",
+    "rle",
+    "rhz",
+]
+
+print("downloading test epochs")
+WR_100_FEATHER_DF = get_demo_df(WR_100_FEATHER)
+assert WR_100_FEATHER_DF.shape == (12425, 51)
+assert all(WR_100_FEATHER_DF.columns == WR_DF_COLS)
+
+P5_1500_FEATHER_DF = get_demo_df(P5_1500_FEATHER)
+assert P5_1500_FEATHER_DF.shape == (335_250, 45)
+assert all(P5_1500_FEATHER_DF.columns == P5_DF_COLS)
+
+print("ok")
+
+
+# @pytest.mark.parametrize(
+#     "_f,h5_group", [[P3_F, "p3"], [P5_F, "p5"], [WR_F, "wr"]]
+# )
+# def test__hdf_read_epochs(_f, h5_group):
+#     epoch_id, time = "epoch_id", "time_ms"
+#     epochs_df = epf._hdf_read_epochs(
+#         DATA_DIR / _f, h5_group, epoch_id=epoch_id, time=time,
+#     )
+#     with pytest.raises(ValueError) as excinfo:
+#         epf._hdf_read_epochs(
+#             DATA_DIR / _f, h5_group=None, epoch_id=epoch_id, time=time
+#         )
+#     assert "You have to give h5_group key" in str(excinfo.value)
+#     _ = epf._epochs_QC(
+#         epochs_df, epochs_df.columns.tolist(), epoch_id=epoch_id, time=time
+#     )
+
+
+# test default, alternative, and None epoch_id, time
+@pytest.mark.parametrize(
+    "_epoch_id,_time",
+    [
+        (epf.EPOCH_ID, epf.TIME),
+        (epf.EPOCH_ID + "_ALT", epf.TIME + "_ALT"),
+        pytest.param(None, None, marks=pytest.mark.xfail()),
+    ],
+)
+def test__validate_epochs_df(_epoch_id, _time):
+
+    # these should succeed
+    epochs_df, channels = fake_data._generate(
+        n_epochs=10,
+        n_samples=100,
+        n_categories=2,
+        n_channels=32,
+        time=_time,
+        epoch_id=_epoch_id,
+    )
+
+    # defaults without the optional args
+    if _epoch_id == epf.EPOCH_ID and _time == epf.TIME:
+        epf._validate_epochs_df(epochs_df)
+
+    # explicit args that match the data
+    epf._validate_epochs_df(epochs_df, epoch_id=_epoch_id, time=_time)
+
+    # these should fail ... mismatches
+    with pytest.raises(Exception) as excinfo:
+        xfail_epochs_df, channels = fake_data._generate(
+            n_epochs=10,
+            n_samples=100,
+            n_categories=2,
+            n_channels=32,
+            time="xfail",
+            epoch_id="xfail",
+        )
+        epf._validate_epochs_df(xfail_epochs_df, epoch_id=_epoch_id, time=_time)
+    if not (excinfo.type is ValueError and "column not found" in excinfo.value.args[0]):
+        raise Exception(f"uncaught exception {excinfo}")
+
+
+# test by using one file
+def test_epochs_QC():
+    epochs_df = WR_100_FEATHER_DF.copy()
+    data_streams = ["MiPf", "MiCe", "MiPa", "MiOc"]
+    epf._epochs_QC(epochs_df, data_streams, epoch_id="epoch_id", time="time_ms")
+
+
+def test_epochs_QC_fails():
+    epochs_df = WR_100_FEATHER_DF.copy()
+    data_streams = ["MiPf", "MiCe", "MiPa", "MiOc"]
+
+    with pytest.raises(ValueError) as excinfo:
+        epochs_df1 = [1, 2]
+        epf._epochs_QC(epochs_df1, data_streams)
+    assert "epochs_df must be a Pandas DataFrame." in str(excinfo.value)
+
+    with pytest.raises(ValueError) as excinfo:
+        data_streams1 = set(data_streams)
+        epf._epochs_QC(epochs_df, data_streams1)
+    assert "data_streams should be a list of strings." in str(excinfo.value)
+
+    with pytest.raises(ValueError) as excinfo:
+        data_streams1 = ["A"]
+        epf._epochs_QC(epochs_df, data_streams1)
+    assert "data_streams should all be present in the epochs dataframe," in str(
+        excinfo.value
+    )
+
+
+def test_raises_error_on_duplicate_channels():
+
+    epochs_table, channels = fake_data._generate(
+        n_epochs=10,
+        n_samples=100,
+        n_categories=2,
+        n_channels=32,
+        time=TIME,
+        epoch_id=EPOCH_ID,
+    )
+    dupe_channel = channels[0]
+    dupe_column = epochs_table[dupe_channel]
+    bad_epochs_table = pd.concat([epochs_table, dupe_column], axis=1)
+
+    with pytest.raises(ValueError) as excinfo:
+        epf._epochs_QC(bad_epochs_table, channels)
+    assert "Duplicate column names" in str(excinfo.value)
+
+
+def test_epochs_unequal_snapshots():
+
+    epochs_table, channels = fake_data._generate(
+        n_epochs=10,
+        n_samples=100,
+        n_categories=2,
+        n_channels=32,
+        time=TIME,
+        epoch_id=EPOCH_ID,
+    )
+
+    epochs_table.drop(epochs_table.index[42], inplace=True)
+    with pytest.raises(ValueError) as excinfo:
+        epf._epochs_QC(epochs_table, channels)
+    assert "differs from previous snapshot" in str(excinfo.value)
+
+
+def test_duplicate_values_of_epoch_id():
+    epochs_table, channels = fake_data._generate(
+        n_epochs=10,
+        n_samples=100,
+        n_categories=2,
+        n_channels=32,
+        time=TIME,
+        epoch_id=EPOCH_ID,
+    )
+    epochs_table.loc[epochs_table[EPOCH_ID] == 16, [EPOCH_ID]] = 18
+
+    with pytest.raises(ValueError) as excinfo:
+        epf._epochs_QC(epochs_table, channels)
+    assert "Duplicate values of epoch_id" in str(excinfo.value)
+
+
+@pytest.mark.parametrize(
+    "data_streams",
+    [
+        ["channel0", "channel1"],
+        pytest.param("channel_xfail", marks=pytest.mark.xfail(strict=True)),
+        pytest.param("[channel_xfail]", marks=pytest.mark.xfail(strict=True)),
+    ],
+)
+@pytest.mark.parametrize(
+    "_epoch_id",
+    ["epoch_id", pytest.param("epoch_id_xfail", marks=pytest.mark.xfail(strict=True)),],
+)
+@pytest.mark.parametrize(
+    "_time", ["time", pytest.param("time_xfail", marks=pytest.mark.xfail(strict=True))],
+)
+def test_check_epochs(data_streams, _epoch_id, _time):
+    """test UI wrapper for epochs QC"""
+
+    # build default fake data
+    epochs_table, channels = fake_data._generate(
+        n_epochs=10,
+        n_samples=100,
+        n_categories=2,
+        n_channels=2,
+        time=TIME,
+        epoch_id=EPOCH_ID,
+    )
+
+    # some succeed, some xfail
+    epf.check_epochs(epochs_table, data_streams, _epoch_id, _time)
+
+
+def test__find_subscript():
+    time = "time"
+    epochs_df = fake_data._get_df()
+    times = epochs_df[time].unique()
+    start = 6
+    stop = 8
+    with pytest.raises(ValueError) as excinfo:
+        istart, istop = epf._find_subscript(times, start, stop)
+    assert "start is too large" in str(excinfo.value)
+    start = -3
+    stop = -1
+    with pytest.raises(ValueError) as excinfo:
+        istart, istop = epf._find_subscript(times, start, stop)
+    assert "stop is too small" in str(excinfo.value)
+    start = 4
+    stop = 2
+    with pytest.raises(ValueError) as excinfo:
+        istart, istop = epf._find_subscript(times, start, stop)
+    assert "Bad rescaling slice" in str(excinfo.value)
+
+
+def test_center_eeg():
+    epochs_df = fake_data._get_df()
+
+    # save a copy for demonstration
+    # check_epochs_df = epochs_df.copy()
+
+    # center two columns in place (for demonstration start and stop are epoch row indexes not times)
+    eeg_streams = ["x", "z"]
+    epoch_id = "epoch_id"
+    time = "time"
+    start = 0
+    stop = 2
+
+    # verify centering == 0 and report failures
+    n_times = len(epochs_df[time].unique())
+    n_epochs = len(epochs_df[epoch_id].unique())
+    times = epochs_df[time].unique()
+    istart, istop = epf._find_subscript(times, start, stop)
+    center_idxs = np.array(
+        [
+            np.arange(istart + (i * n_times), istop + (i * n_times))
+            for i in range(n_epochs)
+        ]
+    ).flatten()
+    centered_epochs_df = epf.center_eeg(
+        epochs_df, eeg_streams, start, stop, epoch_id=EPOCH_ID, time="time"
+    )
+    zero_mns = (
+        centered_epochs_df.iloc[center_idxs, :].groupby(epoch_id)[eeg_streams].mean()
+    )
+    assert np.allclose(0, zero_mns)
+    epf._epochs_QC(centered_epochs_df, eeg_streams, epoch_id=epoch_id, time=time)
+
+
+def test_drop_bad_epochs():
+    epoch_id = "epoch_id"
+    time = "time_ms"
+
+    epochs_df = WR_100_FEATHER_DF.copy()
+    bads_column = "eeg_artifact"
+
+    epochs_df_good = epf.drop_bad_epochs(
+        epochs_df, bads_column, epoch_id=epoch_id, time=time
+    )
+    epochs_df_good["new_col"] = 0
+
+    # get the group of time == 0
+    group = epochs_df.groupby([time]).get_group(0)
+    good_idx = list(group[epoch_id][group[bads_column] == 0])
+    epochs_df_bad = epochs_df[~epochs_df[epoch_id].isin(good_idx)]
+    assert epochs_df_good.shape[0] + epochs_df_bad.shape[0] == epochs_df.shape[0]
+    epochs_df_good = epf.drop_bad_epochs(epochs_df, bads_column, epoch_id, time)
+    epf._epochs_QC(
+        epochs_df_good, epochs_df_good.columns.tolist(), epoch_id=epoch_id, time=time,
+    )
+
+
+def test_re_reference():
+
+    # create a fake data
+    epochs_df = pd.DataFrame(
+        np.array([[0, -3, 1, 2, 3], [0, -2, 4, 5, 6], [0, -1, 7, 8, 9]]),
+        columns=[EPOCH_ID, TIME, "a", "b", "c"],
+    )
+
+    eeg_streams = ["b", "c"]
+
+    ref = ["a"]
+    ref_type = "linked_pair"
+    br_epochs_df = epf.re_reference(epochs_df, eeg_streams, ref, ref_type)
+    assert list(br_epochs_df.b) == [1.5, 3.0, 4.5]
+    ref = ["a"]
+    ref_type = "new_common"
+    br_epochs_df = epf.re_reference(epochs_df, eeg_streams, ref, ref_type)
+    assert list(br_epochs_df.b) == [1, 1, 1]
+    ref = ["a", "b"]
+    ref_type = "common_average"
+    br_epochs_df = epf.re_reference(epochs_df, eeg_streams, ref, ref_type)
+    assert list(br_epochs_df.b) == [0.5, 0.5, 0.5]
+
+    with pytest.raises(ValueError) as excinfo:
+        ref1 = set(ref)
+        br_epochs_df = epf.re_reference(epochs_df, eeg_streams, ref1, ref_type)
+    assert "ref should be a list of strings" in str(excinfo.value)
+
+    epf._epochs_QC(br_epochs_df, eeg_streams, epoch_id=EPOCH_ID, time=TIME)
+
+
+@pytest.mark.parametrize(
+    "eeg_streams,ref,ref_type,expected",
+    [
+        (["b", "c"], ["a"], "linked_pair", [1.5, 3.0, 4.5]),
+        (["b", "c"], ["a"], "new_common", [1, 1, 1]),
+        (["b", "c"], ["a", "b"], "common_average", [0.5, 0.5, 0.5]),
+        pytest.param(
+            ["b", "c"],
+            ["a", "b"],
+            "Xcommon_average",
+            [0.5, 0.5, 0.5],
+            marks=pytest.mark.xfail(raises=ValueError),
+        ),
+    ],
+)
+def test_re_reference_2(eeg_streams, ref, ref_type, expected):
+
+    # create a fake data
+    epochs_df = pd.DataFrame(
+        np.array([[0, -3, 1, 2, 3], [0, -2, 4, 5, 6], [0, -1, 7, 8, 9]]),
+        columns=[EPOCH_ID, TIME, "a", "b", "c"],
+    )
+
+    br_epochs_df = epf.re_reference(epochs_df, eeg_streams, ref, ref_type)
+
+    assert list(br_epochs_df.b) == expected
+    epf._epochs_QC(br_epochs_df, eeg_streams, epoch_id=EPOCH_ID, time=TIME)
+
+
+@pytest.mark.parametrize(
+    "trim_edges,df_shape", [(False, (335_250, 45)), (True, (253_896, 45))]
+)
+def test_fir_filter_epochs(trim_edges, df_shape):
+
+    epoch_id = "epoch_id"
+    time = "time_ms"
+
+    epochs_df = P5_1500_FEATHER_DF.copy()
+
+    eeg_cols = ["MiPf", "MiCe", "MiCe", "MiOc"]
+    epf.check_epochs(epochs_df, data_streams=eeg_cols, epoch_id=epoch_id, time=time)
+
+    _fp = dict(
+        ftype="lowpass",
+        cutoff_hz=12.5,
+        width_hz=5,
+        ripple_db=60,
+        window="kaiser",
+        sfreq=250,
+    )
+
+    filt_test_df = epf.fir_filter_epochs(
+        epochs_df, eeg_cols, trim_edges=trim_edges, epoch_id=epoch_id, time=time, **_fp,
+    )
+    epf.check_epochs(filt_test_df, data_streams=eeg_cols, epoch_id=epoch_id, time=time)
+
+    filt_times = filt_test_df[time].unique()
+    if trim_edges is False:
+        assert all(filt_times == epochs_df[time].unique())
+        assert filt_test_df.shape == df_shape
+    else:
+        assert filt_test_df.shape == df_shape
+
+        # slice epochs_df to match the filtered one
+        qstr = f"{time} in @filt_times"
+        epochs_df = epochs_df.query(qstr)
+        assert filt_test_df.shape == df_shape == epochs_df.shape
+
+    # check the filter changed all and only selected columns
+    assert isinstance(filt_test_df, pd.DataFrame)
+    assert all(filt_test_df.columns == epochs_df.columns)
+    for col in epochs_df.columns:
+        if col in eeg_cols:
+            assert not all(epochs_df[col] == filt_test_df[col])
+        else:
+            assert all(epochs_df[col] == filt_test_df[col])
---
+++ tests/test_filters.py
@@ -0,0 +1,325 @@
+import pandas as pd
+import numpy as np
+import pytest
+import spudtr.filters as filters
+import matplotlib.pyplot as plt
+
+
+xfve = pytest.mark.xfail(strict=True, reason=ValueError)
+
+
+@pytest.mark.parametrize("_allow_defaults", [True, pytest.param(False, marks=xfve)])
+@pytest.mark.parametrize(
+    "_ftype,_cutoff, _srate",
+    [
+        ("lowpass", 20.0, 250.0),
+        ("highpass", 10.0, 250.0),
+        ("bandpass", [15.0, 20.0], 250.0),
+        ("bandstop", [20.0, 30.0], 250.0),
+        pytest.param(None, 20.0, 250, marks=xfve),
+        pytest.param("Xlowpass", 20.0, 250.0, marks=xfve),
+        pytest.param("lowpass", None, 250, marks=xfve),
+        pytest.param("lowpass", "_nn", 250, marks=xfve),
+        pytest.param("lowpass", 20.0, None, marks=xfve),
+        pytest.param("lowpass", 20.0, "_nn", marks=xfve),
+    ],
+)
+def test_check_filter_params_obligatory(_ftype, _cutoff, _srate, _allow_defaults):
+    filt_params = filters.check_filter_params(
+        ftype=_ftype, cutoff_hz=_cutoff, sfreq=_srate, allow_defaults=_allow_defaults
+    )
+
+
+@pytest.mark.parametrize("_width_hz", [5.0, None, pytest.param("_nn", marks=xfve)])
+@pytest.mark.parametrize("_ripple_db", [53.0, None, pytest.param("_nn", marks=xfve)])
+@pytest.mark.parametrize("_window", ["kaiser", "hamming", "hann", "blackman"])
+def test_check_filter_params_defaults(_width_hz, _ripple_db, _window):
+    filt_params = filters.check_filter_params(
+        ftype="lowpass",
+        cutoff_hz=25.0,
+        sfreq=25,
+        width_hz=_width_hz,
+        ripple_db=_ripple_db,
+        window=_window,
+        allow_defaults=True,
+    )
+
+
+def test__suggest_epoch_length():
+    sfreq = 250
+    ripple_db = 60
+    width_hz = 4
+    N = filters._suggest_epoch_length(
+        sfreq=sfreq, width_hz=width_hz, ripple_db=ripple_db
+    )
+    assert N == 230
+
+
+@pytest.mark.parametrize(
+    "_ftype,_cutoff_hz",
+    [
+        ("lowpass", 25.0),
+        ("highpass", 10.0),
+        ("bandpass", [10, 15]),
+        ("bandstop", [10, 15]),
+    ],
+)
+@pytest.mark.parametrize("_window", (None, "kaiser", "hamming", "hann", "blackman"))
+def test_show_filter(_ftype, _cutoff_hz, _window):
+
+    sfreq = 250
+    width_hz = 5.0
+    ripple_db = 60.0
+
+    print("test", locals())
+    filters.show_filter(
+        ftype=_ftype,
+        cutoff_hz=_cutoff_hz,
+        sfreq=sfreq,
+        width_hz=width_hz,
+        ripple_db=ripple_db,
+        window=_window,
+    )
+
+    filters.show_filter(ftype=_ftype, cutoff_hz=_cutoff_hz, sfreq=sfreq)
+    filters.show_filter(
+        ftype=_ftype, cutoff_hz=_cutoff_hz, sfreq=sfreq, show_output=False
+    )
+    plt.close("all")
+
+
+@pytest.mark.parametrize(
+    "_ftype,_cutoff_hz",
+    [
+        ("lowpass", 12.5),
+        ("highpass", 20),
+        ("bandpass", [10, 20]),
+        ("bandstop", [5, 10]),
+    ],
+)
+@pytest.mark.parametrize("_window", ("kaiser", "hamming", "hann", "blackman"))
+def test_fir_filter_dt(_ftype, _cutoff_hz, _window):
+    _params = filters.check_filter_params(
+        ftype=_ftype,
+        cutoff_hz=_cutoff_hz,
+        window=_window,
+        sfreq=250,
+        allow_defaults=True,
+    )
+
+    # create data vector, dataframe, structured array,
+    t, y = filters._sins_test_data([10], [1.0])  # freqs, amps
+    test_df = pd.DataFrame({"fakedata": y})
+    struct_arry = np.array(y, dtype=np.dtype([("fakedata", float)]))
+
+    # dataframes and structured arrays should be OK
+    for dt in [test_df, struct_arry]:
+        filt_dt = filters.fir_filter_dt(dt, ["fakedata"], **_params)
+        assert type(filt_dt) == type(dt)
+        assert dt.shape == filt_dt.shape
+        if isinstance(dt, pd.DataFrame):
+            assert filt_dt.columns.tolist() == ["fakedata"]
+        elif isinstance(dt, np.ndarray):
+            assert filt_dt.dtype.names == ("fakedata",)
+
+    # np.array should fail
+    with pytest.raises(TypeError) as excinfo:
+        filt_dt = filters.fir_filter_dt(y, ["fakedata"], **_params)
+    assert "dt must be pandas.DataFrame or structured numpy.ndarray" in str(
+        excinfo.value
+    )
+
+
+def test_mfreqz():
+    ripple_db = 60.0
+    sfreq = 250
+
+    # low pass
+    cutoff_hz = 10.0
+    width_hz = 5.0
+    ftype = "lowpass"
+    window = "hamming"
+    taps = filters._design_firwin_filter(
+        ftype=ftype,
+        cutoff_hz=cutoff_hz,
+        sfreq=sfreq,
+        width_hz=width_hz,
+        ripple_db=ripple_db,
+        window=window,
+    )
+    _ = filters._mfreqz(
+        b=taps, a=1, cutoff_hz=cutoff_hz, sfreq=sfreq, width_hz=width_hz
+    )
+    assert len(taps) == 183
+
+    # bandstop
+    ftype = "bandstop"
+    window = "hann"
+    cutoff_hz = [18, 35]
+    width_hz = 5
+    taps2 = filters._design_firwin_filter(
+        ftype=ftype,
+        cutoff_hz=cutoff_hz,
+        sfreq=sfreq,
+        width_hz=width_hz,
+        ripple_db=ripple_db,
+        window=window,
+    )
+    _ = filters._mfreqz(
+        b=taps2, a=1, cutoff_hz=cutoff_hz, sfreq=sfreq, width_hz=width_hz
+    )
+
+
+def test_impz():
+    cutoff_hz = 10.0
+    width_hz = 5.0
+    ripple_db = 60.0
+    sfreq = 250
+    ftype = "lowpass"
+    window = "hamming"
+
+    taps = filters._design_firwin_filter(
+        ftype=ftype,
+        sfreq=sfreq,
+        cutoff_hz=cutoff_hz,
+        width_hz=width_hz,
+        ripple_db=ripple_db,
+        window=window,
+    )
+    fig = filters._impz(b=taps, a=1)
+    assert len(taps) == 183
+
+
+@pytest.mark.parametrize(
+    "_ftype,_cutoff_hz,_width_hz,_window",
+    [
+        ("highpass", 20, 5, "blackman"),
+        ("highpass", 20, 4, "kaiser"),
+        ("bandpass", [22, 40], 5, "kaiser"),
+        ("bandpass", [22, 40], 5, "hann"),
+        ("bandstop", [18, 35], 5, "kaiser"),
+    ],
+)
+def test_design_firwin_filter(_ftype, _cutoff_hz, _width_hz, _window):
+    ripple_db = 60
+    sfreq = 250
+    taps = filters._design_firwin_filter(
+        ftype=_ftype,
+        cutoff_hz=_cutoff_hz,
+        sfreq=sfreq,
+        width_hz=_width_hz,
+        ripple_db=ripple_db,
+        window=_window,
+    )
+
+
+def test__apply_firwin_filter_data():
+    # creat a fakedata to show the filter
+    freq_list = [10, 25, 45]
+    amplitude_list = [1.0, 1.0, 1.0]
+    t, y = filters._sins_test_data(freq_list, amplitude_list)
+
+    _params = filters.check_filter_params(
+        ftype="bandstop",
+        window="hann",
+        cutoff_hz=[18, 35],
+        width_hz=5,
+        ripple_db=60,
+        sfreq=250,
+    )
+
+    # build and apply the filter
+    taps = filters._design_firwin_filter(**_params)
+    filt_data = filters._apply_firwin_filter_data(y, taps)
+    assert len(taps) == 183
+
+    # test low pass fails when cutoff has two bounds
+    freq_list = [0.2, 3]
+    amplitude_list = [1.0, 1.0]
+    sampling_freq = 250
+    t, y = filters._sins_test_data(freq_list, amplitude_list, sampling_freq)
+
+    _params = filters.check_filter_params(
+        ftype="lowpass", cutoff_hz=1, sfreq=sampling_freq, allow_defaults=True
+    )
+    with pytest.raises(ValueError) as excinfo:
+        y_filt = filters.fir_filter_data(y, **_params)
+    assert "filter I/O length mismatch" in str(excinfo.value)
+
+
+@pytest.mark.parametrize("window_type", ("kaiser", "hamming", "hann", "blackman"))
+def test_fir_filter_data(window_type):
+    # creat a fakedata to show the filter
+    freq_list = [10, 25, 45]
+    amplitude_list = [1.0, 1.0, 1.0]
+    t, data = filters._sins_test_data(freq_list, amplitude_list)
+
+    ftype = "bandstop"
+    window = "hann"
+    cutoff_hz = [18, 35]
+    width_hz = 5
+    ripple_db = 60
+    sfreq = 250
+
+    filt_data = filters.fir_filter_data(
+        data,
+        cutoff_hz=cutoff_hz,
+        sfreq=sfreq,
+        ftype=ftype,
+        width_hz=width_hz,
+        ripple_db=ripple_db,
+        window=window_type,
+    )
+
+    # fetch
+    _params = filters.check_filter_params(
+        ftype=ftype, cutoff_hz=cutoff_hz, sfreq=sfreq, allow_defaults=True
+    )
+    filt_data = filters.fir_filter_data(data, **_params)
+
+
+@pytest.mark.parametrize("_show_plot", [True, False])
+def test_sins_test_data(_show_plot):
+    # creat a fakedata to show the filter
+    freq_list = [10, 30]
+    amplitude_list = [1.0, 1.0]
+    t, y = filters._sins_test_data(freq_list, amplitude_list, show_plot=_show_plot)
+    assert len(t) == 375
+
+
+@pytest.mark.parametrize(
+    "_ft,_cthz,_win,_ripdb",
+    [
+        ("bandstop", [18, 35], "kaiser", 53),
+        ("highpass", 12.5, "hann", 44),
+        ("bandstop", [18, 35], "blackman", 74),
+    ],
+)
+def test__trans_bwidth_ripple2(_ft, _cthz, _win, _ripdb):
+    sfreq = 250
+    width_hz, ripple_db = filters._trans_bwidth_ripple(
+        ftype=_ft, cutoff_hz=_cthz, sfreq=sfreq, window=_win
+    )
+    assert ripple_db == _ripdb
+
+
+@pytest.mark.parametrize(
+    "_ftype,_cutoff_hz",
+    [["lowpass", 20], ["highpass", 20], ["bandpass", [22, 40]], ["bandstop", [18, 35]]],
+)
+def test_filters_effect(_ftype, _cutoff_hz):
+
+    width_hz = 5
+    ripple_db = 60
+    window = "blackman"
+    sfreq = 250
+    filters.filters_effect(
+        ftype=_ftype,
+        cutoff_hz=_cutoff_hz,
+        width_hz=width_hz,
+        ripple_db=ripple_db,
+        window=window,
+        sfreq=sfreq,
+    )
+    plt.clf()
+    plt.close("all")
